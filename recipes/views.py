ef1ec0119bfa1c6f2701a5a5651bb32adcf0c6f6
OrlandoNeto024 <orlandoneto024@yahoo.com>
2022-11-10 03:04:20 -0300

    testing

--- recipes/views.py
+++ recipes/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def home(request):
-    return render(request, 'global/home.html')
+    return render(request, 'recipes/home.html')
 
 def contato(request):
     return HttpResponse("Contato")
