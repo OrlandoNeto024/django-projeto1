c7df26e816aa1cfe71b45413200fa094b2c8fac3
OrlandoNeto024 <orlandoneto024@yahoo.com>
2022-11-13 15:14:43 -0300

    recipes urls with int ids

--- recipes/views.py
+++ recipes/views.py
@@ -3,4 +3,7 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
     return render(request, 'recipes/pages/home.html')
+def recipe(request, id):
+    return render(request, 'recipes/pages/home.html') 
+
 
