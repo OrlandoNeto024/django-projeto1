2e5541de32707deab48c1348c82aa28705ab43fe
OrlandoNeto024 <orlandoneto024@yahoo.com>
2022-11-14 16:23:52 -0300

    detail page + conditions

--- recipes/views.py
+++ recipes/views.py
@@ -4,12 +4,13 @@ from .utils.recipes.factory import make_recipe
 # Create your views here.
 def home(request):
     return render(request, 'recipes/pages/home.html', context={
-        'recipes': [make_recipe() for _ in range(10)],
+        'recipes': [make_recipe() for _ in range(12)],
     })
 
 def recipe(request, id):
     return render(request, 'recipes/pages/recipe-view.html', context={
         'recipe': make_recipe(),
+        'is_detail_page': True
     }) 
 
 
